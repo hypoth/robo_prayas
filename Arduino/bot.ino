8684c1aed4535525e1b06c933c0d718991d267bb
hypoth <vishal.pathania@gmail.com>
2026-07-14 11:05:26 +0530

    version 1 of Arduino implementation

---
+++ Arduino/bot.ino
@@ -0,0 +1,209 @@
+#include <Servo.h>
+
+// ---------------- Motor Pins ----------------
+const int motorLeftPin1 = 9;
+const int motorLeftPin2 = 10;
+const int ena = 11;
+
+const int motorRightPin1 = 2;
+const int motorRightPin2 = 4;
+const int enb = 3;
+
+// ---------------- Ultrasonic Sensor ----------------
+const int trigPin = 8;
+const int echoPin = 7;
+
+// ---------------- Servo ----------------
+const int radarServoPin = 5;
+const int vehicleRadarAxisDifferenceAngle = 90; // angle the servo should be in for vehile logtitudnal axis be in line with radar axis.
+
+Servo radar;
+
+// ---------------- Distance Limits ----------------
+const float minDistance = 10.0;
+const float minSafeDistance = 25.0;
+const int sensorDurationMax = 25000;
+
+const int traceBackSpeed = 70;
+const int botTurningSpeed = 75;
+const int defaultforwardSpeed = 80;
+int currentMotorSpeed = 0;
+
+void setup()
+{
+  pinMode(motorLeftPin1, OUTPUT);
+  pinMode(motorLeftPin2, OUTPUT);
+  pinMode(ena, OUTPUT);
+
+  pinMode(motorRightPin1, OUTPUT);
+  pinMode(motorRightPin2, OUTPUT);
+  pinMode(enb, OUTPUT);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  radar.attach(radarServoPin);
+  radar.write(vehicleRadarAxisDifferenceAngle);          // Face forward
+  delay(200);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int forwardSpeed = defaultforwardSpeed; 
+  float frontDistance = measureDistance();
+  delay(80);
+
+  
+  Serial.println("Front Clearance: " + String(frontDistance));
+	if(frontDistance > 0) {
+    if(frontDistance <= minSafeDistance){
+      stopRobot();
+      delay(200);
+
+      backward(traceBackSpeed);
+      delay(400);
+
+      stopRobot();
+      delay(200);
+
+      // Look Left
+      radar.write(vehicleRadarAxisDifferenceAngle-90);
+      delay(300);
+      float left = measureDistance();
+      Serial.println("Left Side clearance: " + String(left));
+      // Look Right
+      for(int i = 0; i<=vehicleRadarAxisDifferenceAngle+90; i=i+30)
+      {
+        radar.write(i);
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(100);
+      }
+      float right = measureDistance();
+      Serial.println("Right Side clearance: " + String(right));
+
+      // Center Servo
+      radar.write(vehicleRadarAxisDifferenceAngle);
+      delay(300);
+
+      if(left > right) // turn in direction where you have longer visibility
+      {
+        turnLeft(botTurningSpeed);
+        delay(800);
+        Serial.println("Turning Left with Speed: " + String(botTurningSpeed));
+      }
+      else
+      {
+        turnRight(botTurningSpeed);
+        delay(800);
+        Serial.println("Turning Right with Speed: " + String(botTurningSpeed));
+      }
+
+      stopRobot();
+      delay(200);
+    }
+  } 
+  
+  if(frontDistance > minSafeDistance){
+    if(forwardSpeed != currentMotorSpeed) {
+  	  forward(forwardSpeed);
+      delay(200);
+      Serial.println("Moving Forward with Speed: " + String(forwardSpeed));
+    }
+  }  
+  
+}
+
+// ---------------- Distance Function ----------------
+
+float measureDistance()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH, sensorDurationMax);
+  float distance = 0;
+  if(duration < sensorDurationMax){
+  	//distance = duration / 58.0; //change is to multiplicaiton
+    distance = duration * 0.0172; //change is to multiplicaiton
+    
+  }
+  return distance;
+}
+
+// ---------------- Robot Movements ----------------
+
+void forward(int speed)
+{
+  digitalWrite(motorLeftPin1, LOW);
+  digitalWrite(motorLeftPin2, HIGH);
+
+  digitalWrite(motorRightPin1, LOW);
+  digitalWrite(motorRightPin2, HIGH);
+
+  analogWrite(ena, speed);
+  analogWrite(enb, speed);
+  currentMotorSpeed = speed;
+}
+
+void backward(int speed)
+{
+  digitalWrite(motorLeftPin1, HIGH);
+  digitalWrite(motorLeftPin2, LOW);
+
+  digitalWrite(motorRightPin1, HIGH);
+  digitalWrite(motorRightPin2, LOW);
+
+  analogWrite(ena, speed);
+  analogWrite(enb, speed);
+  currentMotorSpeed = speed;
+}
+
+void turnLeft(int speed)
+{
+  digitalWrite(motorLeftPin1, HIGH);
+  digitalWrite(motorLeftPin2, LOW);
+
+  digitalWrite(motorRightPin1, LOW);
+  digitalWrite(motorRightPin2, HIGH);
+
+  analogWrite(ena, speed);
+  analogWrite(enb, speed);
+  currentMotorSpeed = speed;
+}
+
+void turnRight(int speed)
+{
+  digitalWrite(motorLeftPin1, LOW);
+  digitalWrite(motorLeftPin2, HIGH);
+
+  digitalWrite(motorRightPin1, HIGH);
+  digitalWrite(motorRightPin2, LOW);
+
+  analogWrite(ena, speed);
+  analogWrite(enb, speed);
+  currentMotorSpeed = speed;
+}
+
+void stopRobot()
+{
+  analogWrite(ena, 0);
+  analogWrite(enb, 0);
+
+  digitalWrite(motorLeftPin1, LOW);
+  digitalWrite(motorLeftPin2, LOW);
+
+  digitalWrite(motorRightPin1, LOW);
+  digitalWrite(motorRightPin2, LOW);
+  currentMotorSpeed = 0;
+}
+	
--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 This document details the mathematical framework, calculations, and lookup tables required to make a two-wheel differential drive robot which can execute precise in-place turns, move forward and backwards while avoiding obstacles.
 
 
+## Simulation
+The robot started as simple simulatios on online tool "TinkerCad". A basic version of the robt was made with components available for simulation on TinkerCad. 
+
+### Simulation of the robot
+https://www.tinkercad.com/things/lA5BwXk2kSj-diffdrivebotver1/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard&sharecode=8Be7qrpKp4Ho1W9cGgfTjN29vAA4K9HrsxYx8px5Kfg
+
+### Simulation Schematics
+The schematics for the simulation can be found at /schematics/DiffDriveBot.pdf
+
+### Robot Schematics
+Actual robot is constructed from the same schematicsis /schematics/DiffDriveBot.pdf . 
+
+The key difference from schematics is 
+* ** Robot uses LN298 D dual motor power driver.**
+* ** 12 V Li Ion Battery pack is used for making the system.**
+
+## Robot Construction
+The robot consists of following main parts.
+* **  **
+* **  **
+* **  **
+* **  **
+ 
